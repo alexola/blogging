d6399d06b89463408ff9f94a9a28a91aa4904df4
alexo <alexola.22@gmail.com>
2022-06-01 15:18:21 +0100

    styling for the card WIP

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,14 +1,29 @@
 import Link from 'next/link'
+import styles from '../styles/BlogCard.module.css'
+
 
 export function BlogCard({title, author, coverPhoto, datePublished, slug}) {
   return(
     <div className={styles.card}>
-      <Link>
+      <Link href={'/posts/' + slug} >
         <div className={styles.imgContainer}>
           <img src={coverPhoto.url} alt="" />
         </div>
       </Link>
+      <div className={styles.text}>
+        <h2>{title}</h2>
+        <div className={styles.details}>
+          <div classNme={styles.author}>
+          <img src={author.avatar} alt={author.name} />
+            <h3>{author.name}</h3>
+          </div>
+          <div className={styles.date} >
+            <h3>{datePublished}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
